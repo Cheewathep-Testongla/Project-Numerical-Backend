c37b8bf18bea49c53dfc8db0b4aa6b67ebe51534
s6204062616049 <nongton2544@gmail.com>
2022-05-27 00:05:00 +0700

    Deploy Backend

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const app = jsonServer.create()
 const router = jsonServer.router('./Chapter.json')
 const middlewares = jsonServer.defaults({noCors:true})
 var cors = require("cors");
-// const rules = auth.rewriter({
-//     "root-equation":660,
-//     "linear":660,
-//     "polation":660,
-// })
+const rules = auth.rewriter({
+    "Numerical_Method":660
+})
 app.db = router.db
 app.use(cors())
-// app.use(rules)
+app.use(rules)
 app.use(auth)
 app.use(middlewares);
+
+// Swagger Doc
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./Cheewathep-Testongla-NumericalAPI-0.1-resolved.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(router)
 app.listen(3001,()=>
 {
